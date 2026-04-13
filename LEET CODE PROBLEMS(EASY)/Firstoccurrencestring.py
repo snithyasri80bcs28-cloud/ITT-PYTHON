b69efb75592191d509fd36276a507478b7946352
snithyasri80bcs28-cloud <snithyasri80_bcs28@mepcoeng.ac.in>
2026-04-13 10:28:51 +0530

    Firstoccurrencestring.py

---
+++ LEET CODE PROBLEMS(EASY)/Firstoccurrencestring.py
@@ -0,0 +1,12 @@
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        if needle=="":
+            return 0
+        else:
+            for i in range(len(haystack)):
+                if haystack[i]==needle[0]:
+                    if haystack[i:i+len(needle)]==needle:
+                        return i
+            
+            return -1
+        
